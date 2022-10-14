7db6696a0ae8e2f435fb86a5b23db9d8fb37d00c
plsnotjoe <96671050+plsnotjoe@users.noreply.github.com>
2022-10-14 12:00:42 +0100

    init commit

---
+++ src/compiler.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+class Tokenizer
+    TOKEN_TYPES = [
+        [:def,          /\bfunc\b/],
+        [:end,          /\bend\b/],
+        [:identifier,   /\b[a-zA-Z]+\b/],
+        [:integer,      /\b[0-9]+\b/],
+        [:oparen,       /\(/],
+        [:cparen,       /\)/],
+    ]
+
+    def initialize(code)
+        @code = code
+    end
+
+    def tokenize
+        tokens = []
+
+        until @code.empty?
+            tokens << tokenize_one_token
+            @code = @code.strip
+        end
+
+        return tokens
+    end
+
+    def tokenize_one_token
+        TOKEN_TYPES.each do |type, re|
+            re = /\A(#{re})/
+            if @code =~ re
+                value = $1
+                @code = @code[value.length..-1]
+                return Token.new(type, value)
+            end
+        end
+
+        raise RuntimeError.new("Couldn't match token on the #{@code.inspect}")
+    end
+end
+
+class Parser
+    def initialize()
+        @tokens = tokens
+    end
+
+    def parse
+        
+    end
+end
+
+Token = Struct.new(:type, :value)
+
+tokens = Tokenizer.new(File.read("test.qs")).tokenize
+puts tokens.map(&:inspect).join("\n")
+tree = Parser.new(tokens).parse
+p tree
